ba72b7963f0ac1e324a7c843035e1469ff9bbe37
sslees <sam@sslees.com>
2022-12-18 14:56:45 -0700

    finished chapt 19

--- memory.c
+++ memory.c
@@ -1,5 +1,7 @@
 #include "memory.h"
 
+#include "vm.h"
+
 #include <stdlib.h>
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
@@ -11,4 +13,24 @@ void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
   void* result = realloc(pointer, newSize);
   if (result == NULL) exit(1);
   return result;
+}
+
+static void freeObject(Obj* object) {
+  switch (object->type) {
+    case OBJ_STRING: {
+      ObjString* string = (ObjString*)object;
+      FREE_ARRAY(char, string->chars, string->length + 1);
+      FREE(ObjString, object);
+      break;
+    }
+  }
+}
+
+void freeObjects() {
+  Obj* object = vm.objects;
+  while (object != NULL) {
+    Obj* next = object->next;
+    freeObject(object);
+    object = next;
+  }
 }
--- value.c
+++ value.c
@@ -1,8 +1,10 @@
 #include "value.h"
 
 #include "memory.h"
+#include "object.h"
 
 #include <stdio.h>
+#include <string.h>
 
 void initValueArray(ValueArray* array) {
   array->values = NULL;
@@ -32,6 +34,7 @@ void printValue(Value value) {
     case VAL_BOOL: printf(AS_BOOL(value) ? "true" : "false"); break;
     case VAL_NIL: printf("nil"); break;
     case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
+    case VAL_OBJ: printObject(value); break;
   }
 }
 
@@ -41,6 +44,12 @@ bool valuesEqual(Value a, Value b) {
     case VAL_BOOL: return AS_BOOL(a) == AS_BOOL(b);
     case VAL_NIL: return true;
     case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
+    case VAL_OBJ: {
+      ObjString* aString = AS_STRING(a);
+      ObjString* bString = AS_STRING(b);
+      return aString->length == bString->length &&
+             memcmp(aString->chars, bString->chars, aString->length) == 0;
+    }
     default: return false; // Unreachable.
   }
 }
