7b1f53a3a47d086858d10fccc0e5f92af71ddc89
sslees <sam@sslees.com>
2022-12-23 11:41:49 -0800

    finished chapt 29

--- debug.c
+++ debug.c
@@ -78,6 +78,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
       return constantInstruction("OP_GET_PROPERTY", chunk, offset);
     case OP_SET_PROPERTY:
       return constantInstruction("OP_SET_PROPERTY", chunk, offset);
+    case OP_GET_SUPER:
+      return constantInstruction("OP_GET_SUPER", chunk, offset);
     case OP_EQUAL: return simpleInstruction("OP_EQUAL", offset);
     case OP_GREATER: return simpleInstruction("OP_GREATER", offset);
     case OP_LESS: return simpleInstruction("OP_LESS", offset);
@@ -94,6 +96,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
     case OP_LOOP: return jumpInstruction("OP_LOOP", -1, chunk, offset);
     case OP_CALL: return byteInstruction("OP_CALL", chunk, offset);
     case OP_INVOKE: return invokeInstruction("OP_INVOKE", chunk, offset);
+    case OP_SUPER_INVOKE:
+      return invokeInstruction("OP_SUPER_INVOKE", chunk, offset);
     case OP_CLOSURE: {
       offset++;
       uint8_t constant = chunk->code[offset++];
@@ -116,6 +120,7 @@ int disassembleInstruction(Chunk* chunk, int offset) {
       return simpleInstruction("OP_CLOSE_UPVALUE", offset);
     case OP_RETURN: return simpleInstruction("OP_RETURN", offset);
     case OP_CLASS: return constantInstruction("OP_CLASS", chunk, offset);
+    case OP_INHERIT: return simpleInstruction("OP_INHERIT", offset);
     case OP_METHOD: return constantInstruction("OP_METHOD", chunk, offset);
     default: printf("Unknown opcode %d\n", instruction); return offset + 1;
   }
